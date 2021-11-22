63aaa13e5fe12d56e5d00966006043be99631e63
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-22 15:05:16 +0100

    con while al posto di for

--- js/script.js
+++ js/script.js
@@ -5,21 +5,26 @@ var applicazione = new Vue ({
     },
     mounted(){
         const self = this;
-        for (let i = 0 ; i < 10 ; i++){
-        axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function(risultato){
-        let email = risultato.data.response;
-        self.emails.push(email);
-        })
+        // for (let i = 0 ; i < 10 ; i++){
+        // axios
+        // .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        // .then(function(risultato){
+        // let email = risultato.data.response;
+        // self.emails.push(email);
+        // })
+        // }
+
+        let i = 0;
+        while ( i < 10 ){
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((risultato) => {
+                console.log('then');
+                let email = risultato.data.response;
+                this.emails.push(email);
+            });
+            i++;
         }
-        
-                
-                
-            
-            
-            
-        
         
     }
 
